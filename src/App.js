c98570668ed1c3f98ca2a0bf319b05d6848f656e
Sveake <51757935+Sveake@users.noreply.github.com>
2021-06-14 19:54:45 +0300

    Set up search engine with alert

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       Hello
-      </header>
+      <div className="container">
+        <main>
+          <Dictionary />
+        </main>
+       <footer className="App-footer"> <small>Coded by Svea Lillepalu</small></footer>
+       </div>
     </div>
   );
 }
